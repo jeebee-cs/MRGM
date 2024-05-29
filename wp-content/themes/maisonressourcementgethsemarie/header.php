557087f0bb45f36aae884d6b94e12ded7c64b3a4
Gabriel Bélanger <131696525+gabriel-belanger@users.noreply.github.com>
2024-05-28 22:39:51 -0400

    Remove : waiting html for when the js is up

--- wp-content/themes/maisonressourcementgethsemarie/header.php
+++ wp-content/themes/maisonressourcementgethsemarie/header.php
@@ -37,6 +37,3 @@
 			<?php wp_nav_menu(["menu" => "Header"]) ?>
 		</div>
 	</header>
-	<div id="wait-js-over">
-		<svg class="ring" viewBox="25 25 50 50" stroke-width="3"> <circle cx="50" cy="50" r="20" /></svg>
-	</div>
